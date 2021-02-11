2636c36dc87d15f690cea9f67209fb7ca5a25a82
Arsalan <Arsalanbardsiri@gmail.com>
2021-02-11 12:53:23 -0800

    Tutorial #1
    An Intro to objects and classes, and using methods

---
+++ Tutorial1.py
@@ -0,0 +1,20 @@
+#Classes and objects
+import turtle
+
+x = 5
+y = 'String'
+
+print(type(x))
+print(type(y))
+
+print(y.replace('s'.upper(),''))
+
+"""
+creating a new instance of turtle object,
+turtle.Turtle() constructor
+so t = new turtle object
+"""
+
+t = turtle.Turtle()
+
+#Also know the differece between functions and methods
