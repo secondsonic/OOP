55ad28e130b4d87a24d2d7a5b3280cbc0c727ad3
Arsalan <Arsalanbardsiri@gmail.com>
2021-02-11 22:30:08 -0800

    Added examples
    Added more prints and stuff for better understanding

--- Tutorial4.py
+++ Tutorial4.py
@@ -10,6 +10,7 @@ class Point():
     def move(self, x, y):
         self.x += x
         self.y += y
+        print(self.cords, 'Moves to:', self)
 
     def __add__(self, p):
         return Point(self.x + p.x, self.y + p.y)
@@ -48,12 +49,19 @@ p2 = Point(3,2)
 p3 = Point(1,3)
 p4 = Point(0,1)
 p8 = Point(3,4)
+
 print()
 
 p5 = p1 + p2
 p6 = p4 - p1
 p7 = p2 * p3
 
+print(p7)
+
+p2.move(1,2)
+p4.move(1,0)
+newp5 = p1 + p2
+newp6 = p4 - p1
 # print(p5, p6, p7)
 print(p1 == p2)
 print(p1 == p8)
